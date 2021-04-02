b24d5da8e3cd7974b9beca5c6402a8e7158f7bd5
Abdi-Isse <abdi.isse96@gmail.com>
2021-04-02 22:09:26 +0800

    Add User Authentication to web app

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from website import create_app
+from website import db, create_app, models
 
 app = create_app()
+with app.app_context():
+    db.create_all()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port= 5000, debug=True)
