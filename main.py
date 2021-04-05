ffed56c70393b1bfaf06be4344896fc9aba9f3da
Farshad Ghanbari <farshad.ghanbari@hotmail.com>
2021-04-05 17:33:57 +0800

    added comments for mac and linux

--- main.py
+++ main.py
@@ -5,4 +5,8 @@ with app.app_context():
     db.create_all()
 
 if __name__ == '__main__':
+    # code for mac
     app.run(host='0.0.0.0', port= 5000, debug=True)
+
+    # code for Linux
+    # app.run(debug=True)
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,1 +1,5 @@
+# code for mac
 pip3 install -r ./requirements.txt
+
+# code for Linux
+# pip3 install -r ../requirements.txt
