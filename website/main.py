050ee2bf551e2bead3505f5bf8bdc88ada3d53e7
lukesalter71 <40336854+lukesalter71@users.noreply.github.com>
2021-05-04 22:50:40 +0800

    Update main.py

--- website/main.py
+++ website/main.py
@@ -21,4 +21,14 @@ def courses():
     return render_template('readmore.html')
 
 
+#Quiz taking view function (todo)
 @main.route('/quiz/<quizid>', methods=['GET','POST'])
+@login_required
+def quiz(quizid):
+    questions = db.Question_Bank.query.filter_by(id=quizid) #extract the given quiz's questions from the question_bank table into an array.
+    currentquestion = 0
+    question = questions[currentquestion]
+    answers = []
+    for i in question.answers:
+        answers.append(i.answer_text)
+    return ()
