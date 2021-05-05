cb983a8db29a12bb848f4ca4509a82a0b1d400d4
lukesalter71 <40336854+lukesalter71@users.noreply.github.com>
2021-05-05 13:56:32 +0800

    Update forms.py
    added explanatory comments

--- website/forms.py
+++ website/forms.py
@@ -6,6 +6,6 @@ from wtforms.validators import DataRequired, ValidationError, Optional, Length
 
 # Form for taking question inputs on Course Quizzes.
 class Question(FlaskForm):
-    choices = RadioField('Answer', validators = [Optional()])
-    answer_text = TextAreaField('Answer', validators = [Optional(), Length(max=500)])
-    submit = SubmitField("Submit")
+    choices = RadioField('Answer', validators = [Optional()]) #only is populated if the question is MCQ
+    answer_text = TextAreaField('Answer', validators = [Optional(), Length(max=500)]) #only is visible if the quesiton is text-answer.
+    submit = SubmitField("Submit") 
