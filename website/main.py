d15c1262eb5067e30fa302c45122d6d8819b43e2
lukesalter71 <40336854+lukesalter71@users.noreply.github.com>
2021-05-05 18:43:23 +0800

    Branch2 lukesalter71 viewfunction (#26)
    * Update main.py
    
    * Update main.py
    
    * Update main.py
    
    * Update forms.py
    
    Removed unnecessary import
    
    * Update forms.py
    
    added explanatory comments
    
    * Update main.py
    
    Added view function - quiz:
    
    not entirely finished:
    still have to implement actions for GET requests.
    
    Commented out redirect to results screen as it is currently not ready.
    
    * Update main.py
    
    added code for GET requests, commented it out since 'quiz.html' is not ready yet.
    
    * Update main.py
    
    Removed unnecessary commenting

--- website/main.py
+++ website/main.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session, redirect, url_for
 from flask_login import login_required, current_user
+from . import db, forms
 
 main = Blueprint('main', __name__)
 
@@ -21,3 +22,36 @@ def courses():
 
 
 #Quiz taking view function (todo)
+@main.route('/quiz/<quizid>', methods=['GET','POST'])
+@login_required
+def quiz(quizid):
+    if session['question_number'] == -1:                                                                
+        session['question_number'] = 0 
+        session.modified = True
+    questions = db.Question_Bank.query.filter_by(id=quizid)                                             
+    question = questions[session['question_number']]
+    answers = []
+    for i in question.answers:                                                                         
+        answers.append(i.answer_text)
+    
+    #grab the form object:
+    question_form = forms.Question()
+
+    if question.question_type == 2:                                                                     
+        question_form.choices = answers                                                                 
+
+    if question_form.validate_on_submit():                                                              
+
+        if question.question_type == 2:
+            user_answer = question_form.choices.data                                                    
+        elif question.question_type == 1:                                                              
+            user_answer = question_form.answer_text.data
+
+        #if int(session['question_number'])+1 == len(questions):                                         
+            #return(redirect(url_for('marks', quizid = quizid)))                                         
+
+        session['question_number'] += 1
+        session.modified = True
+        return(redirect(url_for('quiz',quizid = quizid))) 
+
+    return() #(render_template('quiz.html', title="Quiz", question_form=question_form,question = question))  -- Commented out since 'quiz.html' is not ready.
